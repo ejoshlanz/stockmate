bdd908da61b2f2ac56b76d02d5233b7c59a2f7e4
Joshua <ejoshlanzuela@gmail.com>
2025-03-18 13:40:19 +0800

    initial commit
    # Conflicts:
    #	app/build.gradle
    #	app/src/main/java/com/joshlanz/stockmate/MainActivity.java
    #	app/src/main/res/layout/activity_main.xml
    #	app/src/main/res/values-night/colors.xml
    #	app/src/main/res/values/colors.xml
    #	app/src/main/res/values/strings.xml

--- app/src/main/java/com/joshlanz/stockmate/SalesPage.java
+++ app/src/main/java/com/joshlanz/stockmate/SalesPage.java
@@ -8,11 +8,11 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 
-public class MainActivity extends AppCompatActivity {
+public class SalesPage extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.sales_page);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
